1b722931e2a396b769a1bacbf265cdc7d017efb4
Filcer <gabfiltzer@gmail.com>
2016-09-17 21:24:31 -0500

    Update Hanoi.cpp

--- src/Hanoi.cpp
+++ src/Hanoi.cpp
@@ -1,11 +1,7 @@
-//Hanoi.cpp
-
-//cuando se imprima la torre tiee que haber una linea entre cada paso
-
+//cuando se imprima la torre tiene que haber una linea entre cada paso
 
 /** Nombre(s):
 Descripción: ________
-
 
 **/
 
@@ -29,11 +25,66 @@ void Hanoi::Resolver()
 		{
 			int O=(this->origen.empty())?0:this->origen.top();
 			int A=
-
-
 		}
 	}
-
-
-
 }
+
+
+void Hanoi::LlenarTorre() //¿incompleto?
+{
+	for(int i= this->Discos;i>0;--i)
+	{
+		this->origen.push(i);
+	}
+}
+
+void Hanoi::MostrarTorres()
+{
+	std::stack<int> aux0;
+	std::stack<int> auxA;
+	std::stack<int> auxD;
+	int mayor;
+	if(this->origen.size() >this->auxiliar.size() && this->origen.size() >this->destino.size())
+		{
+			mayor=origen.size();
+		}else if(this->auxiliar.size() >this->origen.size() && this->auxiliar.size > this->destino.size())
+		{
+			mayor=auxiliar.size();
+		}else
+		{
+			mayor=destino.size();
+		}
+		for(int i=mayor; i>0; --i)
+		{
+			if(i<=origen.size())
+			{
+				std::cout<<origen.top()<<" "
+				aux0.push(origen.top());
+				origen.pop();
+			}else
+			{
+				std::cout<<"*"<<" "
+			}
+
+			if(i<=auxiliar.size())
+			{
+				std::cout<<auxiliar.top()<<" "
+				auxA.push(auxiliar.top());
+				auxiliar.pop();
+			}else
+			{
+				std::cout<<"*"<<" "
+			}
+			if(i<=destino.size())
+			{
+				std::cout<<destino.top()<<std::endl;
+				auxD.push(destino.top());
+				destino.pop();
+			}else
+			{
+				std::cout<<"*"<<endl; 
+			}
+		}
+}
+
+
