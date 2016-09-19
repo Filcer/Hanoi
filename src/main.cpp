6eebf3fd3f8f3ba9a2aad0110a774f0ec6b51f7b
Filcer <gabfiltzer@gmail.com>
2016-09-19 01:52:28 -0500

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,43 @@
-/**
- *
- *
- *
- */
+//Main.cpp
 
-#include "Hanoi.h"
+#include <Hanoi.h>
 
 int main()
 {
-	cout<<"\tTorres de Hanoi"<<endl;
-	Hanoi H(3);
-	H.llenar(3);
-	H.mostrar();
-	H.resolver();
-	H.mostrar();
+	int discos, opcion;
+	std::cout<<"Torres de Hanoi"<<std:endl;
+	std::cout<<"Ingrese el Numero de discos: "<<std:endl;
+	cin>>discos;
+	Hanoi H=Hanoi(discos);
+	do{
+		cout << "1. Mostrar" <<endl;
+    		cout << "2. Resolver" <<endl;
+    		cout << "3. Llenar Torres" <<endl;
+    		cout << "0. Salir del Programa..." <<endl;
+    		cin >> opcion;
+	switch(opcion){
+		case 1:
+			H.MostrarTorres();
+		break;
+		
+		case 2:
+			H.Resolver();
+			H.MostrarTorres(); //para ver si se resolvio correctamente
+		break;
+		
+		case 3:
+			std::cout<<"Ingrese el Numero de discos: "<<std:endl;
+			cin>>discos;
+			H.LlenarTorre(discos);
+		break;
+		
+		case 0:
+		break;
+		default:
+            	std::cout << "Opcion incorrecta." << std::endl;
+		}
+	} while (opcion != 0);
+
 	return 0;
 }
 
