b1d7ea5d3c6ce676c7442286cf12b65f6c8cce7d
Filcer <gabfiltzer@gmail.com>
2016-09-19 01:42:19 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,21 @@
 
 int main()
 {
-	int discos;
+	int discos, opcion;
 	std::cout<<"Torres de Hanoi"<<std:endl;
 	std::cout<<"Ingrese el Numero de discos: "<<std:endl;
 	cin>>discos;
 	Hanoi H(discos);
+	switch(opcion){
+		case 1:
+		
+	
 	H.LlenarTorre();
 	H.MostrarTorres();
 	H.Resolver();
 	H.MostrarTorres();
+	}
+	
 	return 0;
 }
 
